3d9fa26af2cbabc2cfcadf80df97f60c2ed69880
serignmbaye <barsaliou1998@gmail.com>
2023-03-30 17:29:00 +0000

    feat: apply stats when number of element >= 10

--- student/main.js
+++ student/main.js
@@ -14,20 +14,32 @@ const rl = readline.createInterface({
 rl.on('line', (line) => {
     // Parsing the user input as an integer
     const num = parseInt(line);
+    let lowerLimit = 0
+    let upperLimit = 0
 
     // Adding the parsed number to the `numbers` array
     numbers.push(num);
 
-    // Calculating the mean (average) of the numbers array
-    const average = numbers.reduce((acc, val) => acc + val) / numbers.length;
+    if (numbers.length <= 10) {
+        // Take big range for 1rst value
+        lowerLimit = num - 100;
+        upperLimit = num + 100;
+    } else {
+        // If there are more than 5 elements in the data array, get the last 5 elements and calculate the mean and standard deviation of those elements
+        const data = numbers.slice(-10);
 
-    // Calculating the standard deviation of the numbers array
-    const standardDeviation = Math.sqrt(numbers.reduce((acc, val) => acc + Math.pow(val - average, 2)) / numbers.length);
+        // Calculating the mean (average) of the numbers array
+        const average = data.reduce((acc, val) => acc + val) / data.length;
 
-    // Calculating the lower and upper limits for the range
-    const lowerLimit = Math.round(average - 1.28 * standardDeviation);
-    const upperLimit = Math.round(average + 1.28 * standardDeviation);
+        // Calculating the standard deviation of the numbers array
+        const standardDeviation = Math.sqrt(data.reduce((acc, val) => acc + Math.pow(val - average, 2)) / data.length);
+
+        // Calculating the lower and upper limits for the range
+        lowerLimit = Math.round(average - 1.28 * standardDeviation);
+        upperLimit = Math.round(average + 1.28 * standardDeviation);
+    }
 
     // Printing the calculated lower and upper limits
     console.log(`${lowerLimit} ${upperLimit}`);
+
 });
